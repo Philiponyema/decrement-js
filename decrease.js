faafb8b93fed70e8ae0def6b8d930ed5a102e4a3
philiponyema <slanxywhite@yahoo.com>
2024-01-20 04:29:18 -0800

    first commit

---
+++ decrease.js
@@ -0,0 +1,15 @@
+function increaseNum(){
+    let getInput = document.getElementById('form-input');
+   let num = getInput.innerHTML
+    num = parseInt(num) + 1
+    getInput.innerHTML = num
+}
+
+function decreaseNum(){
+    let getInput = document.getElementById('form-input');
+   let num = getInput.innerHTML
+    num = parseInt(num) + 1
+    getInput.innerHTML = num
+}
+
+
